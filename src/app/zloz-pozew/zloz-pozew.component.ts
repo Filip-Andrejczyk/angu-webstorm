58b150eb6539cbec5b4e35ff9b926269749ed9a7
Filip-Andrejczyk <filip.andrejczyk@student.wat.edu.pl>
2021-09-06 15:37:38 +0200

    dodane wartosci do wyboru w formularzu

--- src/app/zloz-pozew/zloz-pozew.component.ts
+++ src/app/zloz-pozew/zloz-pozew.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-zloz-pozew',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ZlozPozewComponent implements OnInit {
 
-  constructor() { }
+  public pozewFormularz: FormGroup = this.fb.group({});
+
+  constructor(private fb: FormBuilder) { }
+
+  adresatPisma: string[] = ["I Komisariat Policji W Białymstoku",
+                            "II Komisariat Policji W Białymstoku",
+                            "III Komisariat Policji W Białymstoku",
+                            "IV Komisariat Policji W Białymstoku",
+                            "Komenda Miejska Policji",
+                            "Marszałek Sejmu",
+                            "Prezydent Miasta Białystok",
+                            "Premier",
+                            "Sąd Rejonowy w Białymstoku",
+                            "Sąd Apelacyjny w Białymstoku",
+                            "Wojewódzki Sąd Administracyjny w Białymstoku",
+                            "Samodzielny Publiczny Psychiatryczny Zakład Opieki Zdrowotnej w Choroszczy"]
+
+  rodzajPisma: string[] = ["Pismo", "Pozew", "Skarga", "Donos", "Zażalenie", "Wniosek"]
 
   ngOnInit(): void {
+    this.pozewFormularz = this.fb.group({
+      autor: "",
+      rodzajPisma: this.rodzajPisma[0],
+      adresat: this.adresatPisma[0],
+      sprawaTemat: "",
+      opis: ""
+    });
+  }
+
+  initilizePozewForm(): void{
+
   }
 
 }
