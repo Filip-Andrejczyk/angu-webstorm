862e9a0d1a4d9cd2cadfc16a0fa7ed6701a399d9
Filip-Andrejczyk <filip.andrejczyk@student.wat.edu.pl>
2021-09-30 14:53:26 +0200

    dodany modul i component

---
+++ src/app/pismogame/pismogame.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameComponent } from './game/game.component';
+
+
+
+@NgModule({
+  declarations: [
+    GameComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PismogameModule { }
